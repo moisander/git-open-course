b566d926654a84117d4678ae0fbed726d38b7b58
Iiro Moisander <iiro.moisander@aiven.io>
2023-12-03 18:40:02 +0200

    fix hello_world

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,2 @@
 print("Hello world!")
-<<<<<<< HEAD
-print("Hello from feature")
-print("Hello from release")
-=======
 print("Hello once again")
->>>>>>> intermediate-normal-merge/feature/add-logging
